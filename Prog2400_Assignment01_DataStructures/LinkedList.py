4b7e3a452a9581121f06bb6894ea5c594fcffecb
Meagan Townsend <meagantownsend337@gmail.com>
2024-03-21 12:14:54 -0300

    Added requirements for assignment 02

--- Prog2400_Assignment01_DataStructures/LinkedList.py
+++ Prog2400_Assignment01_DataStructures/LinkedList.py
@@ -3,6 +3,7 @@ class Node:
         self.value = value
         self.next = None
 
+
 class LinkedList:
     def __init__(self):
         self.head = None
@@ -21,63 +22,138 @@ class LinkedList:
 
     def prepend(self, value):
         new_node = Node(value)
-        if not self.head:
-            self.head = new_node
+        new_node.next = self.head
+        self.head = new_node
+        if self._size == 0:
             self.tail = new_node
-        else:
-            new_node.next = self.head
-            self.head = new_node
         self._size += 1
+
+    def insert_at_position(self, value, position):
+        if position < 0 or position > self._size:
+            return "Position out of bounds"
+        if position == 0:
+            self.prepend(value)
+            return
+        if position == self._size:
+            self.append(value)
+            return
+        new_node = Node(value)
+        temp = self.head
+        for _ in range(position - 1):
+            temp = temp.next
+        new_node.next = temp.next
+        temp.next = new_node
+        self._size += 1
+
+    def delete_first_node(self):
+        if not self.head:
+            return "List is empty"
+        self.head = self.head.next
+        if self._size == 1:
+            self.tail = None
+        self._size -= 1
+
+    def delete_last_node(self):
+        if not self.head:
+            return "List is empty"
+        if self._size == 1:
+            self.head = None
+            self.tail = None
+        else:
+            temp = self.head
+            while temp.next != self.tail:
+                temp = temp.next
+            temp.next = None
+            self.tail = temp
+        self._size -= 1
+
+    def delete_at_position(self, position):
+        if position < 0 or position >= self._size:
+            return "Position out of bounds"
+        if position == 0:
+            self.delete_first_node()
+            return
+        temp = self.head
+        for _ in range(position - 1):
+            temp = temp.next
+        temp.next = temp.next.next
+        if position == self._size - 1:
+            self.tail = temp
+        self._size -= 1
 
     def size(self):
         return self._size
 
-    def get_head(self):
-        return self.head
-
-    def get_tail(self):
-        return self.tail
-
-    def at(self, index):
-        if index < 0 or index >= self._size:
-            return None
-        current = self.head
-        for _ in range(index):
-            current = current.next
-        return current
-
-    def pop(self):
-        if not self.head:
-            return None
-        if self.head == self.tail:
-            value = self.head.value
-            self.head = None
-            self.tail = None
-            self._size -= 1
-            return value
-        current = self.head
-        while current.next != self.tail:
-            current = current.next
-        value = self.tail.value
-        current.next = None
-        self.tail = current
-        self._size -= 1
-        return value
-
-    def contains(self, value):
+    def print_list(self):
         current = self.head
         while current:
-            if current.value == value:
-                return True
+            print(current.value, end=" -> " if current.next else "")
             current = current.next
-        return False
+        print()
 
-    def find(self, value):
-        current = self.head
-        index = 0
-        while current:
-            if current.value == value:
-                return index
-            current = current.next
-            index += 1
-        return None
+    def split_list(self):
+        if not self.head or not self.head.next:
+            return self.head, None
+        slow = self.head
+        fast = self.head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        front = self.head
+        back = slow.next
+        slow.next = None
+        return front, back
+
+    def merge_sorted_lists(self, l1, l2):
+        dummy = Node(0)
+        tail = dummy
+        while l1 and l2:
+            if l1.value < l2.value:
+                tail.next, l1 = l1, l1.next
+            else:
+                tail.next, l2 = l2, l2.next
+            tail = tail.next
+        tail.next = l1 or l2
+        return dummy.next
+
+    @staticmethod
+    def merge_sort_list(head):
+        if not head or not head.next:
+            return head
+        middle = LinkedList.get_middle(head)
+        next_to_middle = middle.next
+        middle.next = None
+        left = LinkedList.merge_sort_list(head)
+        right = LinkedList.merge_sort_list(next_to_middle)
+        sorted_list = LinkedList().merge_sorted_lists(left, right)
+        return sorted_list
+
+    @staticmethod
+    def get_middle(head):
+        if not head:
+            return head
+        slow = head
+        fast = head
+        while fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
+
+    def sort(self):
+        self.head = LinkedList.merge_sort_list(self.head)
+
+
+# Helper functions to convert between linked lists and Python lists for demonstration
+def list_to_linked_list(elements):
+    ll = LinkedList()
+    for element in elements:
+        ll.append(element)
+    return ll
+
+
+def linked_list_to_list(node):
+    result = []
+    while node:
+        result.append(node.value)
+        node = node.next
+    return result
--- Prog2400_Assignment01_DataStructures/Main.py
+++ Prog2400_Assignment01_DataStructures/Main.py
@@ -1,16 +1,65 @@
 from LinkedList import LinkedList
 
-# Create a new instance of LinkedList
-my_list = LinkedList()
+# Example usage demonstrating all functionalities
+if __name__ == "__main__":
+    # Create a new LinkedList instance
+    ll = LinkedList()
+    print("After appending 10, 20, 30:")
+    ll.append(10)
+    ll.append(20)
+    ll.append(30)
+    ll.print_list()
 
-# Append some values to the list
-my_list.append(10)
-my_list.append(20)
-my_list.append(30)
+    print("Size of the list:", ll.size())
 
-# Print the size of the list
-print("Size of the list:", my_list.size())
+    # Demonstrating insertion at the beginning
+    print("\nAfter prepending 5:")
+    ll.prepend(5)
+    ll.print_list()
 
-# Print the first and last elements of the list
-print("First element:", my_list.head.value)
-print("Last element:", my_list.tail.value)
+    # Demonstrating insertion at a given position
+    print("\nAfter inserting 15 at position 2:")
+    ll.insert_at_position(15, 2)
+    ll.print_list()
+
+    # Demonstrating deletion of the first node
+    print("\nAfter deleting the first node:")
+    ll.delete_first_node()
+    ll.print_list()
+
+    # Demonstrating deletion of the last node
+    print("\nAfter deleting the last node:")
+    ll.delete_last_node()
+    ll.print_list()
+
+    # Demonstrating deletion at a given position
+    print("\nAfter deleting node at position 1:")
+    ll.delete_at_position(1)
+    ll.print_list()
+
+    # Splitting the list
+    front, back = ll.split_list()
+    front_list = LinkedList()
+    front_list.head = front
+    back_list = LinkedList()
+    back_list.head = back
+
+    print("\nFront half of the list:")
+    front_list.print_list()
+
+    print("Back half of the list:")
+    back_list.print_list()
+
+    # Merging two sorted lists
+    list_a = list_to_linked_list([1, 3, 5])
+    list_b = list_to_linked_list([2, 4, 6])
+    merged_list = LinkedList()
+    merged_list.head = merged_list.merge_sorted_lists(list_a.head, list_b.head)
+    print("\nMerged sorted lists:")
+    merged_list.print_list()
+
+    # Sorting the list
+    unsorted_list = list_to_linked_list([3, 1, 4, 1, 5, 9, 2, 6])
+    unsorted_list.sort()
+    print("\nSorted list:")
+    unsorted_list.print_list()
